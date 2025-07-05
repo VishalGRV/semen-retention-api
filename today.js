43fb80d57b5215e6e6d6bf3143c798dbc8bf8087
VishalGRV <49804433+VishalGRV@users.noreply.github.com>
2025-07-05 21:52:27 +0530

    fix:  prevents new quote when refreshed

--- today.js
+++ today.js
@@ -1,16 +1,30 @@
 const quoteText = document.querySelector(".quote-text");
 const quoteUrlField = document.querySelector(".quote-url");
+const quoteAuthor = document.querySelector(".quote-author");
 
 async function loadQuote() {
   try {
     const res = await fetch("http://localhost:8080/random");
-    const data = await res.json();
-    quoteText.innerHTML = `<p style= font-style:bold >"${data.text}"</p> <p style=font-size:15px , font-weight:400 ,>- ${data.author}</p>`;
-    quoteUrlField.value = `http://localhost:8080/quote/${data.id}`;
+    const quote = await res.json();
+    quoteText.textContent = `${quote.text}`;
+    quoteAuthor.textContent = `- ${quote.author}`;
+    quoteUrlField.value = `http://localhost:8080/quote/${quote.id}`;
+    localStorage.setItem("quoteLoaded", "true");
+    localStorage.setItem("quoteText", quote.text);
+    localStorage.setItem("quoteAuthor", quote.author);
+    localStorage.setItem("quoteId", quote.id);
   } catch (error) {
     quoteText.textContent = "Failed to load quote. Try again later.";
     console.error(error);
   }
 }
 
-loadQuote();
+if (!localStorage.getItem("quoteLoaded")) {
+  loadQuote();
+} else {
+  quoteText.textContent = localStorage.getItem("quoteText");
+  quoteAuthor.textContent = `- ${localStorage.getItem("quoteAuthor")}`;
+  quoteUrlField.value = `http://localhost:8080/quote/${localStorage.getItem(
+    "quoteId"
+  )}`;
+}
